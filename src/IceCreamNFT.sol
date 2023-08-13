61cfccdbaa47911fae2c63a64390afa6d83b328a
Darius Foodeei <darius.foodei@gmail.com>
2023-08-14 00:42:57 +0200

    forked + commit of modded code

--- src/IceCreamNFT.sol
+++ src/IceCreamNFT.sol
@@ -1,115 +1,100 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity ^0.8.19;
 
-import {ERC721, ERC721Enumerable} from "openzeppelin/token/ERC721/extensions/ERC721Enumerable.sol";
-import {Ownable} from "openzeppelin/access/Ownable.sol";
-import {Pausable} from "openzeppelin/security/Pausable.sol";
-import "openzeppelin/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./Consumer.sol";
+import "./Inbox.sol";
 import "./Base64.sol";
+// 0x697b6e2a2E975b9469d895c605Cc3262eCeCA416,0x7c97b48e923283b312fd5d378554d8b9e8fe6855
+contract CircleNFT is ERC721Enumerable, GelatoVRFConsumer {
+    using Strings for uint256;
+    using Base64 for bytes; // Use the Base64 library for bytes type
+    uint256 public latestRandomness;
+    GelatoVRFInbox public inbox;
+    address public dedicatedMsgSender;
+    uint256 public _tokenIdCounter;
 
-// solhint-disable
-contract IceCreamNFT is ERC721Enumerable, Ownable, Pausable {
-    mapping(address => bool) public hasClaimed;
-    uint256 public constant W = 1000;
-    uint256 public constant H = 1000;
+    // Mapping from token ID to SVG data
+    mapping(uint256 => string) private _svgData;
 
-    constructor() ERC721("Ice Cream NFT", "LICK") {}
-
-    function pause() external onlyOwner {
-        _pause();
+    constructor(GelatoVRFInbox _inbox, address _dedicatedMsgSender)
+        ERC721("CircleNFT", "CNFT")
+    {
+        inbox = _inbox;
+        dedicatedMsgSender = _dedicatedMsgSender;
     }
 
-    function unpause() external onlyOwner {
-        _unpause();
+    function fullfillRandomness(uint256 randomness, bytes calldata) external {
+        require(msg.sender == dedicatedMsgSender, "The sender is not the VRF");
+        latestRandomness = randomness;
+        
+        uint256 tokenId = _tokenIdCounter;
+        _mint(msg.sender, tokenId);
+        _tokenIdCounter += 1;
+
+        // Create random radius and color for the SVG
+        uint256 radius = randomRadius(latestRandomness);
+        string memory color = randomColor(latestRandomness);
+
+        // Create SVG data
+        string memory svg = createSVG(radius, color);
+        _svgData[tokenId] = svg;
     }
 
-    function _createSVG(string memory body) internal view returns (string memory svgElement) {
-        svgElement = string.concat(
-            '<svg xmlns="http://www.w3.org/2000/svg" ',
-            'width="',
-            Strings.toString(W),
-            '" height="',
-            Strings.toString(H),
-            '"><g>',
-            body,
-            "</g></svg>"
-        );
+    function mintCircle() public {
+        inbox.requestRandomness(this, "");
     }
 
-    function _createCircle(
-        uint256 cx,
-        uint256 cy,
-        uint256 r,
-        string memory fill,
-        string memory stroke,
-        string memory body
-    ) internal pure returns (string memory svgElement) {
-        svgElement = string.concat(
-            "<circle ",
-            'cx ="',
-            Strings.toString(cx),
-            '" ',
-            'cy="',
-            Strings.toString(cy),
-            '" ',
-            'r="',
-            Strings.toString(r),
-            '" ',
-            'fill="',
-            fill,
-            '" ',
-            'stroke="',
-            stroke,
-            '">',
-            body,
-            "</circle>"
-        );
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        string memory svg = _svgData[tokenId];
+        return string(abi.encodePacked("data:image/svg+xml;utf8,", svg));
     }
 
-    function _createAnimation(uint256 cx, uint256 cy, uint256 r) internal pure returns (string memory) {
-        return string.concat(
-            '<animateTransform attributeName="transform" attributeType="XML" type="rotate"',
-            'from="0 ',
-            string.concat(Strings.toString(cx), " ", Strings.toString(cy), '" '),
-            'to="360 ',
-            string.concat(Strings.toString(cx), " ", Strings.toString(cy), '" '),
-            'dur="',
-            Strings.toString(2 + r),
-            '" ',
-            'repeatCount="indefinite"/>'
-        );
+    function randomRadius(uint256 randomness) private pure returns (uint256) {
+        return
+            (uint256(keccak256(abi.encodePacked(randomness))) %
+                50) + 10; // A random value between 10 and 60
     }
 
-    function mint() external {
-        require(!hasClaimed[msg.sender], "already claimed");
-        uint256 nextTokenId = totalSupply() + 1;
-        hasClaimed[msg.sender] = true;
-        _safeMint(msg.sender, nextTokenId);
+    function randomColor(uint256 randomness) private pure returns (string memory) {
+        string[10] memory colors = [
+            "red",
+            "green",
+            "blue",
+            "yellow",
+            "pink",
+            "purple",
+            "orange",
+            "cyan",
+            "magenta",
+            "black"
+        ];
+        uint256 index = uint256(
+            keccak256(abi.encodePacked(randomness))
+        ) % 10;
+        return colors[index];
     }
 
-    function tokenURI(uint256 tokenId_) public view virtual override returns (string memory) {}
-
-    function uint2str(uint256 _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0) {
-            return "0";
-        }
-        uint256 j = _i;
-        uint256 len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint256 k = len;
-        while (_i != 0) {
-            k = k - 1;
-            uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
-            bytes1 b1 = bytes1(temp);
-            bstr[k] = b1;
-            _i /= 10;
-        }
-        return string(bstr);
+    function createSVG(uint256 radius, string memory color)
+        private
+        pure
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(
+                    '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200"><circle cx="100" cy="100" r="',
+                    radius.toString(),
+                    '" fill="',
+                    color,
+                    '"/></svg>'
+                )
+            );
     }
-
-    receive() external payable {}
 }
